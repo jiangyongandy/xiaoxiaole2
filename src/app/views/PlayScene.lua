42eb760588c3becca8afff0933ac2387737b4754
jiangyong <625387857@qq.com>
2017-06-09 17:37:17 +0800

    完成水果高亮，点击消除

--- src/app/views/PlayScene.lua
+++ src/app/views/PlayScene.lua
@@ -40,6 +40,7 @@ function PlayScene:init()
     self.startX = (display.width - self.xCount * FruitItem.getWidth() - (self.xCount - 1) * self.gap)/2
     self.startY = (display.height - self.yCount * FruitItem.getWidth() - (self.yCount - 1) * self.gap)/2 - 30
     self.matrix = {}
+    self.actives = {}
 
     	-- 等待转场特效结束后再加载矩阵
 	--[[ 3.10使用该函数报错
@@ -65,6 +66,7 @@ function PlayScene:initMatrix()
     end
 end
 
+--掉落水果
 function PlayScene:createAndDropFruit(x, y, fruitIndex)
 
     local tempP = self:getFruitPosition(x,y)
@@ -72,7 +74,7 @@ function PlayScene:createAndDropFruit(x, y, fruitIndex)
     local endPosition = cc.p(tempP.x, tempP.y)
 
     local newFruit = FruitItem:create(x,y)
-    self.matrix[ (y - 1) * self.xCount + x ] = newFruit 
+    self.matrix[ self:getMatrixPosition(x,y)] = newFruit 
     newFruit:setPosition(startPosition)
 
     local speed = startPosition.y / (2 * display.height)
@@ -82,10 +84,21 @@ function PlayScene:createAndDropFruit(x, y, fruitIndex)
     newFruit:setTouchEnabled(true)
     newFruit:addClickEventListener(function ()
         --printf("new fruit click")
+        --[[
         if newFruit.isActive then
             newFruit:setActive(false)
         else
             newFruit:setActive(true)
+        end
+        print("初始点击"..newFruit.fruitIndex)
+        ]]
+        if not newFruit.isActive then
+            if self.actives.nums > 0 then
+                self:retainActivies()
+            end
+            self:highLightFruits(newFruit)
+        else
+            self:removeActives()
         end
 
     end)
@@ -93,11 +106,65 @@ function PlayScene:createAndDropFruit(x, y, fruitIndex)
 
 end
 
+--高亮相同水果
+function PlayScene:highLightFruits(fruitItem)
+    
+    fruitItem:setActive(true)
+    table.insert(self.actives,fruitItem)
+    local leftFruit =  self.matrix[ (fruitItem.y - 1) * self.xCount + fruitItem.x - 1 ]
+    local rightFruit =  self.matrix[ (fruitItem.y - 1) * self.xCount + fruitItem.x + 1 ]
+    local topFruit =  self.matrix[ (fruitItem.y - 1 + 1) * self.xCount + fruitItem.x ]
+    local bottomFruit =  self.matrix[ (fruitItem.y - 1 -1) * self.xCount + fruitItem.x]
+    local tempFruitTable = {leftFruit, rightFruit, topFruit, bottomFruit}
+    --越界检查（重要）！！
+    if fruitItem.x - 1 < 1 then 
+        table.remove(tempFruitTable, 1)
+    end
+    if fruitItem.x + 1 > self.xCount then 
+        table.remove(tempFruitTable, 2)
+    end
+    if fruitItem.y + 1 > self.yCount then 
+        table.remove(tempFruitTable, 3)
+    end
+    if fruitItem.y - 1 < 1 then 
+        table.remove(tempFruitTable, 4)
+    end
+    --递归搜索左右上下相同的水果
+    for k,fruit in pairs(tempFruitTable) do
+        if fruit ~= nil and fruit.fruitIndex == fruitItem.fruitIndex and not fruit.isActive then  
+            print("找到一个"..fruit.fruitIndex)
+            self:highLightFruits(fruit)
+        end
+    end
+end
+
+--消除高亮水果
+function PlayScene:removeActives()
+    for k,fruitItem in pairs(self.actives) do
+        self:getResourceNode():removeChild(fruitItem)
+    end
+    self.actives = {}
+end
+
+--恢复高亮水果颜色
+function PlayScene:retainActivies()
+    for k,fruitItem in pairs(self.actives) do
+        FruitItem:setActive(false)
+    end
+    self.actives = {}
+end
+
+--由矩阵坐标得到显示坐标
 function PlayScene:getFruitPosition(x, y)
 
     local px = self.startX + (x - 1)*(FruitItem.getWidth() + self.gap) + FruitItem.getWidth()/2     
     local py = self.startY + (y - 1)*(FruitItem.getWidth() + self.gap) + FruitItem.getWidth()/2
     return cc.p(px, py)
+end
+
+--矩阵坐标转换tableindex
+function PlayScene:getMatrixPosition(x, y)
+    return (y - 1) * self.xCount + x 
 end
 
 return PlayScene
